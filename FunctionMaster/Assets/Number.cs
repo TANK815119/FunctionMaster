9a8282c3f9e3ea14c3aa7e0d813f308c2ef46487
Reid Jacobsen <52799675+TANK815119@users.noreply.github.com>
2025-09-20 18:14:50 -0400

    deleted unecessary scripts made redundant by refactor

--- FunctionMaster/Assets/Number.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Number : MonoBehaviour
-{
-    [field: SerializeField] public int Value { get; private set; }
-}
--- FunctionMaster/Assets/Operation.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Operation : MonoBehaviour
-{
-    [field: SerializeField] public char Value { get; private set; }
-}
