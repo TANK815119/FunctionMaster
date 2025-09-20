598cfbedacd83c26b9a406f03733065a8264fa27
Reid Jacobsen <52799675+TANK815119@users.noreply.github.com>
2025-09-20 16:43:45 -0400

    Operation and Value manipipulation and slots

---
+++ FunctionMaster/Assets/Number.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Number : MonoBehaviour
+{
+    [field: SerializeField] public int Value { get; private set; }
+}
